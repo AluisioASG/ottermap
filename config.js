5c5ce022bcc4eeab3832bac0af64f6fbc1d76b00
Aluísio Augusto Silva Gonçalves <aasg@chirpingmustard.com>
2014-02-06 20:12:34 -0200

    Allow server config to be overridden through npm

--- config.js
+++ config.js
@@ -1,10 +1,22 @@
 var NODE_ENV = process.env.NODE_ENV;
+var npmConfig = function (name) {
+  return process.env['npm_package_config_' + name];
+};
+var openshiftConfig = function (name) {
+  return process.env['OPENSHIFT_' + name];
+};
 
-if ('OPENSHIFT_APP_NAME' in process.env) {
+if ('npm_package_config_override_config' in process.env) {
+  // npm-overridden environment
+  exports.ENVIRONMENT    = NODE_ENV || npmConfig('environment');
+  exports.SERVER_ADDRESS = npmConfig('host') || undefined;
+  exports.SERVER_PORT    = npmConfig('port');
+}
+else if ('OPENSHIFT_APP_NAME' in process.env) {
   // OpenShift (production) environment
   exports.ENVIRONMENT    = NODE_ENV || 'production';
-  exports.SERVER_ADDRESS = process.env['OPENSHIFT_NODEJS_IP'];
-  exports.SERVER_PORT    = process.env['OPENSHIFT_NODEJS_PORT'];
+  exports.SERVER_ADDRESS = openshiftConfig('NODEJS_IP');
+  exports.SERVER_PORT    = openshiftConfig('NODEJS_PORT');
 } else {
   // Local (development) environment
   exports.ENVIRONMENT    = NODE_ENV || 'development';
